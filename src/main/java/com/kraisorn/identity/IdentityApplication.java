7301111be67fa097ef77e1a2faeef26a196db5e3
Kraisorn Apisaranont <kraisornar@gmail.com>
2022-05-12 13:57:18 +0700

    add inspect startup commandline runner for spring boot beans loading

--- src/main/java/com/kraisorn/identity/IdentityApplication.java
+++ src/main/java/com/kraisorn/identity/IdentityApplication.java
@@ -5,7 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class IdentityApplication {
@@ -15,6 +18,21 @@ public class IdentityApplication {
 	}
 
 	private static final Logger log = LoggerFactory.getLogger(IdentityApplication.class);
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
 
 	@Bean
 	public CommandLineRunner demo(PersonRepository repository) {
@@ -53,5 +71,4 @@ public class IdentityApplication {
 			log.info("");
 		};
 	}
-
 }
