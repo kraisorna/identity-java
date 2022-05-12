af7a2d0a7819a513fd48555fb854dfb0a628b3d9
Kraisorn Apisaranont <kraisornar@gmail.com>
2022-05-12 12:18:57 +0700

    identity - create person rest api

---
+++ src/main/java/com/kraisorn/identity/Person.java
@@ -0,0 +1,19 @@
+package com.kraisorn.identity;
+
+public class Person {
+    private final long id;
+    private final String content;
+
+    public Person(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/com/kraisorn/identity/PersonController.java
@@ -0,0 +1,19 @@
+package com.kraisorn.identity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class PersonController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/person")
+    public Person person(@RequestParam(value = "name", defaultValue = "Kraisorn") String name) {
+        return new Person(counter.incrementAndGet(), String.format(template, name));
+    }
+}
