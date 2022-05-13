541886f68a1600176f553f6b012c60aa75e44a24
Kraisorn Apisaranont <kraisornar@gmail.com>
2022-05-13 10:17:16 +0700

    create hypermedia rest api

--- src/main/java/com/kraisorn/identity/controller/PersonController.java
+++ src/main/java/com/kraisorn/identity/controller/PersonController.java
@@ -1,12 +1,17 @@
 package com.kraisorn.identity.controller;
 
+import com.kraisorn.identity.domain.Account;
 import com.kraisorn.identity.exception.PersonNotFoundException;
 import com.kraisorn.identity.repository.PersonRepository;
 import com.kraisorn.identity.domain.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(path="/person")
@@ -18,10 +23,19 @@ public class PersonController {
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/{id}")
-    Person one(@PathVariable Long id) {
+    EntityModel<Person> one(@PathVariable Long id) {
 
-        return repository.findById(id)
+        Person person = repository.findById(id)
                 .orElseThrow(() -> new PersonNotFoundException(id));
+
+        return EntityModel.of(person, //
+                linkTo(methodOn(PersonController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(PersonController.class).getAllPeople()).withRel("person"));
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Person> getAllPeople() {
+        return repository.findAll();
     }
 
     @GetMapping("/add")
